cb33c0e958769dcdb0a58c5883850707db7c11b1
Tim A. Conner <timaconner1@gmail.com>
2017-12-05 10:50:04 -0600

    Add Lightning Exercises 2

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
 /* Exersise 1 */
-let people = [
-    {
-        name: 'Bob', 
-        department: 'sales',
-        title: 'sales manager'},
-    {
-        name: 'Tina', 
-        department: 'finance', 
-        title: 'director of finance'
-    },
-    {
-        name: 'Randy', 
-        department: 'IT', 
-        title: 'hardware guy'},
-    {
-        name: 'Glenda', 
-        department: 'C-suite', 
-        title: 'CEO'
-    }
-];
+// let people = [
+//     {
+//         name: 'Bob', 
+//         department: 'sales',
+//         title: 'sales manager'},
+//     {
+//         name: 'Tina', 
+//         department: 'finance', 
+//         title: 'director of finance'
+//     },
+//     {
+//         name: 'Randy', 
+//         department: 'IT', 
+//         title: 'hardware guy'},
+//     {
+//         name: 'Glenda', 
+//         department: 'C-suite', 
+//         title: 'CEO'
+//     }
+// ];
 
-function print(arr){
-    for(let i = 0; i < arr.length; i++){
-        console.log(`${arr[i].name}’s job title is ${arr[i].title}`);
-    }
-}
+// function print(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         console.log(`${arr[i].name}’s job title is ${arr[i].title}`);
+//     }
+// }
 
-print(people);
+// print(people);
+
+/* Exersise 2 */
+
+var person = (function (originalPerson) {
+    originalPerson.height = "6'3";
+    originalPerson.weight = 175;
+    originalPerson.name = "Tim";
+    let getName = function(){
+        return this.name;
+    }
+
+    originalPerson.getName = getName();
+
+    return originalPerson;
+
+})(person || {});
+
+
+var child = (function (originalPerson) {
+    let favGame = "Stratego";
+    let favFood = "Bread";
+
+    originalPerson.favGame = favGame;
+    originalPerson.favFood = favFood;
+
+    return originalPerson;
+})(person || {});
+
+console.log(child.hasOwnProperty("height"));// True
+
+console.log(child.hasOwnProperty("favGame"));// True
